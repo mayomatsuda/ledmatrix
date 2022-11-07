ccbbec5ce4c556bd2019ba353dcaf8e69b2a030b
Myles MacDonald <mmacd327@uwo.ca>
2022-11-07 12:15:50 -0500

    api class

--- api.cpp
+++ api.cpp
@@ -1,4 +1,8 @@
 #include "api.h"
+#include <sstream>
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Options.hpp>
+#include <curlpp/Easy.hpp>
 using namespace std;
 
 Api::Api(string url) {
@@ -10,5 +14,10 @@ Api::~Api() {
 }
 
 string Api::getData() {
-
+    stringstream response;
+    curlpp::Easy foo;
+    foo.setOpt( new curlpp::options::Url( apiUrl ) );
+    foo.setOpt( new curlpp::options::WriteStream( &response ) );
+    foo.perform();
+    return response.str();
 }
